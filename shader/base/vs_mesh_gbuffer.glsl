66e40223584b53edd5862b00645b6d09e9a9f8ef
cgoxopx <cgoxopx@qq.com>
2026-05-10 23:55:26 +0800

    修改时不隐藏选中的体素

--- shader/base/vs_mesh_gbuffer.glsl
+++ shader/base/vs_mesh_gbuffer.glsl
@@ -7,8 +7,8 @@ uniform vec4 u_depthBias;
 
 void main()
 {
-    gl_Position = mul(u_modelViewProj, vec4(a_position, 1.0));
-    gl_Position.z -= u_depthBias.x * gl_Position.w;
+    vec3 offset_pos = a_position + a_normal * u_depthBias.x;
+    gl_Position = mul(u_modelViewProj, vec4(offset_pos, 1.0));
     v_normal = normalize(mul(u_model[0], vec4(a_normal, 0.0)).xyz);
     v_pos = a_position;
 }
--- ui/render_voxel_render.cpp
+++ ui/render_voxel_render.cpp
@@ -41,12 +41,12 @@ void RenderVoxelList::RenderVoxelItem::render_gbuffer(
         voxel_renderer.renderGBuffer(transform, mesh_shader);
     }
 
-    if (voxel_picking_enabled && !marked_voxels.empty()) {
+    if (!marked_voxels.empty()) {
         if (marked_voxels_dirty) {
             marked_voxels.global_position = voxel_grid_data.global_position;
             marked_voxels.voxel_size = voxel_grid_data.voxel_size;
             marked_mesh_renderer.setBaseColor(1.0f, 0.5f, 0.5f, 1.0f);
-            marked_mesh_renderer.setDepthBias(0.0001f);
+            marked_mesh_renderer.setDepthBias(0.01f);
             int num_triangles = 0;
             auto generator = sinriv::kigstudio::voxel::generateMesh(
                 marked_voxels, 0.5, num_triangles, true);
